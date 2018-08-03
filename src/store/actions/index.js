7bb0a1c4bcf59479c8ccb4be5daa12b466be6af6
nothoutonly <seasilent77@gmail.com>
2018-08-04 00:48:18 +0800

    deploy

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,42 +1,41 @@
-import fetch from "cross-fetch";
+import fetch from 'cross-fetch'
 
-import { ROOT_URL } from "../../config";
+import { ROOT_URL } from '../../config'
 
-export const LOADING_WEATHER_DATA = "LOADING_WEATHER_DATA";
-export const LOADED_WEATHER_DATA = "LOADED_WEATHER_DATA";
-export const ERROR_WEATHER_DATA = "ERROR_WEATHER_DATA";
-export const GET_WEATHER_DATA = "GET_WEATHER_DATA";
+export const LOADING_WEATHER_DATA = 'LOADING_WEATHER_DATA'
+export const LOADED_WEATHER_DATA = 'LOADED_WEATHER_DATA'
+export const ERROR_WEATHER_DATA = 'ERROR_WEATHER_DATA'
+export const GET_WEATHER_DATA = 'GET_WEATHER_DATA'
 
 const requestWeather = city => ({
-  type: LOADING_WEATHER_DATA,
-  payload: city
-});
+	type: LOADING_WEATHER_DATA,
+	payload: city,
+})
 const reseiveWeather = weatherSearchData => ({
-  type: LOADED_WEATHER_DATA,
-  payload: weatherSearchData
-});
+	type: LOADED_WEATHER_DATA,
+	payload: weatherSearchData,
+})
 const fetchWeatherError = error => ({
-  type: ERROR_WEATHER_DATA,
-  payload: error
-});
+	type: ERROR_WEATHER_DATA,
+	payload: error,
+})
 
 export const fetchWeather = city => {
-  return dispatch => {
-    dispatch(requestWeather(city));
-    return fetch(`${ROOT_URL}&q=${city}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Network response was not ok.");
-        }
-      })
-      .then(weatherSearchData => {
-        console.log(weatherSearchData);
-        return dispatch(reseiveWeather(weatherSearchData));
-      })
-      .catch(error => {
-        return dispatch(fetchWeatherError(error));
-      });
-  };
-};
+	return dispatch => {
+		dispatch(requestWeather(city))
+		return fetch(`${ROOT_URL}&q=${city},cn`)
+			.then(response => {
+				if (response.ok) {
+					return response.json()
+				} else {
+					throw new Error('Network response was not ok.')
+				}
+			})
+			.then(weatherSearchData => {
+				return dispatch(reseiveWeather(weatherSearchData))
+			})
+			.catch(error => {
+				return dispatch(fetchWeatherError(error))
+			})
+	}
+}
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,47 +1,46 @@
 import {
-  LOADING_WEATHER_DATA,
-  LOADED_WEATHER_DATA,
-  ERROR_WEATHER_DATA
-} from "../actions/index";
+	LOADING_WEATHER_DATA,
+	LOADED_WEATHER_DATA,
+	ERROR_WEATHER_DATA,
+} from '../actions/index'
 
 const initState = {
-  locationData: "",
-  loading: false,
-  loaded: false,
-  error: false
-};
+	locationData: '',
+	loading: false,
+	loaded: false,
+	error: false,
+}
 
 const rootReducer = function(state = initState, action) {
-  console.log("Action received: ", action);
-  const { payload } = action;
+	const { payload } = action
 
-  switch (action.type) {
-    case LOADING_WEATHER_DATA:
-      return {
-        ...state,
-        locationData: payload,
-        loading: true,
-        loaded: false,
-        error: false
-      };
-    case LOADED_WEATHER_DATA: {
-      const { main, weather, name, wind } = payload;
-      return {
-        ...state,
-        loading: false,
-        loaded: true,
-        error: false,
-        main,
-        weather,
-        name,
-        wind
-      };
-    }
-    case ERROR_WEATHER_DATA:
-      return { ...state, loading: false, loaded: true, error: true };
-    default:
-      return state;
-  }
-};
+	switch (action.type) {
+		case LOADING_WEATHER_DATA:
+			return {
+				...state,
+				locationData: payload,
+				loading: true,
+				loaded: false,
+				error: false,
+			}
+		case LOADED_WEATHER_DATA: {
+			const { main, weather, name, wind } = payload
+			return {
+				...state,
+				loading: false,
+				loaded: true,
+				error: false,
+				main,
+				weather,
+				name,
+				wind,
+			}
+		}
+		case ERROR_WEATHER_DATA:
+			return { ...state, loading: false, loaded: true, error: true }
+		default:
+			return state
+	}
+}
 
-export default rootReducer;
+export default rootReducer
